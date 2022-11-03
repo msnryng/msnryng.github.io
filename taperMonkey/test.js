00e7d3b6225981ff1883caae698d23a890e3faf6
msnryng <94797563+msnryng@users.noreply.github.com>
2022-11-04 03:22:11 +0900

    commit

--- taperMonkey/test.js
+++ taperMonkey/test.js
@@ -6,16 +6,12 @@
 // @author       You
 // @match        https://github.com/msnryng/msnryng.github.io/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @require 	https://msnryng.github.io/taperMonkey/hellotest.js
 // @grant        none
 // ==/UserScript==
 
 (function () {
   "use strict";
-  var script = document.createElement("script");
-  script.type = "text/javascript";
-  script.src = "https://msnryng.github.io/taperMonkey/hellotest.js";
-  var firstScript = document.getElementsByTagName("script")[0];
-  firstScript.parentNode.insertBefore(script, firstScript);
-//   var test = new GitTest("hello dude");
+  var test = new GitTest("hello dude");
   // Your code here...
 })();
