e3418adc4e6686ad90c96bbdbec4cea2b4a274c0
msnryng <94797563+msnryng@users.noreply.github.com>
2022-11-04 03:45:10 +0900

    ok

--- taperMonkey/hellotest.js
+++ taperMonkey/hellotest.js
@@ -1,7 +1,11 @@
-﻿class GitTest {
-  constructor(tex) {
-    console.log("hihi");
-    console.log(tex);
+﻿function getClass() {
+  class GitTest {
+    constructor(tex) {
+      console.log("hihi");
+      console.log(tex);
+    }
   }
+  return GitTest;
 }
+
 window.classLoaded = true;
