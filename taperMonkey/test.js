a56a9e087c3669df2c21e971464b142893c0248f
msnryng <94797563+msnryng@users.noreply.github.com>
2022-11-04 03:40:24 +0900

    ok

--- taperMonkey/test.js
+++ taperMonkey/test.js
@@ -4,14 +4,20 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://github.com/msnryng/msnryng.github.io/*
+// @match        https://nt.site777.tv/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
-// @require 	https://msnryng.github.io/taperMonkey/hellotest.js
 // @grant        none
 // ==/UserScript==
 
 (function () {
-  "use strict";
-  var test = new GitTest("hello dude");
-  // Your code here...
+	"use strict";
+	var script = document.createElement("script")
+	script.type = "text/javascript";
+	script.src =
+    "https://msnryng.github.io/taperMonkey/hellotest.js";
+	var firstScript = document.getElementsByTagName("script")[0];
+	firstScript.parentNode.insertBefore(script, firstScript);
+	while(getClass())
+	var classIs=getClass()
+	var test = new classIs("sssss");
 })();
